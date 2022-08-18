268bf638a16492f0fcb68ed3a2b540c2e3e72b67
MqxS <maxgoog06@outlook.com>
2022-08-18 09:35:35 -0400

    Added time based indexing

--- src/main/java/frc/robot/commands/IndexTeleop.java
+++ src/main/java/frc/robot/commands/IndexTeleop.java
@@ -1,9 +1,7 @@
 package frc.robot.commands;
 
-import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.wpilibj.Encoder;
 import edu.wpi.first.wpilibj.Timer;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.subsystems.Barrel;
 
@@ -11,13 +9,13 @@ public class IndexTeleop extends CommandBase {
 
     private final Barrel barrel;
     private final Encoder encoder;
-    private final int mor = 4;
-    private int counter = 1;
-    private double oldval;
+    private Timer timer;
+    private boolean finished = false;
 
     public IndexTeleop(Barrel barrel) {
         this.barrel = barrel;
         this.encoder = barrel.getBarrelEncoder();
+        timer = new Timer();
         encoder.reset();
         addRequirements(barrel);
     }
@@ -25,31 +23,25 @@ public class IndexTeleop extends CommandBase {
     @Override
     public void initialize() {
         barrel.set(-1);
+        timer.reset();
+        timer.start();
     }
 
     @Override
     public void execute() {
+        if (timer.hasElapsed(0.3)) {
+            finished = true;
+        }
     }
 
     @Override
     public void end(boolean interrupted) {
         barrel.set(0);
-        counter++;
+        finished = false;
     }
 
     @Override
     public boolean isFinished() {
-        SmartDashboard.putNumber("Encoder Distance", encoder.getDistance());
-        SmartDashboard.putNumber("Encoder Raw", encoder.getRaw());
-        SmartDashboard.putNumber("Encoder Old", oldval);
-        oldval = encoder.getDistance();
-        SmartDashboard.putNumber("diff", (encoder.getDistance()-oldval));
-        SmartDashboard.putNumber("Counter", counter);
-        SmartDashboard.putNumber("Margin of Error", mor);
-        SmartDashboard.putNumber("Target Distance", barrel.DISTANCE);
-        SmartDashboard.putBoolean("Finished", (barrel.DISTANCE * counter)+encoder.getDistance() <= mor && (barrel.DISTANCE * counter)+encoder.getDistance() >= -mor);
-
-        return (barrel.DISTANCE * counter)+encoder.getDistance() <= mor && (barrel.DISTANCE * counter)+encoder.getDistance() >= -mor;
+        return finished;
     }
-
 }
