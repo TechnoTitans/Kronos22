4571755c48fea657319d4926864b229ba996293a
MqxS <maxgoog06@outlook.com>
2022-09-19 17:47:01 -0400

    FIXED

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -23,7 +23,7 @@ public class Robot extends TimedRobot {
 
     public static double shoot_delay = 45;
 
-    public static final boolean isController = true;
+    public static final boolean isController = false;
 
     /**
      * This function is run when the robot is first started up and should be used for any
